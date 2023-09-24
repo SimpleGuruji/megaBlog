02f499d0047b953c50f2313b15c5db46eee829b1
Simple Guruji <dashingak957014@gmail.com>
2023-09-24 13:40:26 +0530

    Environment variable work done

---
+++ megaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Mega Project</h1>
+    </>
+  )
+}
+
+export default App
---
+++ megaBlog/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  appwriteApiEndpointId: String(import.meta.env.VITE_APPWRITE_APIENDPOINT_ID),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+};
+
+export default config;
