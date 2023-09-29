6ceff61fb3e2f2b6c4131f28546b5af30d82cd86
Simple Guruji <dashingak957014@gmail.com>
2023-09-29 15:16:30 +0530

    Header and LogoutBtn work completed

---
+++ megaBlog/src/components/index.js
@@ -0,0 +1,14 @@
+import Container from "./Container/Container";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Logo from "./Logo";
+
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo
+    
+    
+}
--- megaBlog/src/store/store.js
+++ megaBlog/src/store/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit"
+import authReducer from "./authSlice"
 const store= configureStore({
-    reducer:{
+    reducer:{authReducer
 
     }
 })
