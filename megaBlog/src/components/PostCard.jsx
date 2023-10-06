87e58bc542ecd5a57ddf47b5541b9cc90322390e
Simple Guruji <dashingak957014@gmail.com>
2023-10-07 01:33:17 +0530

    Login component created using react-hook-form

---
+++ megaBlog/src/components/PostCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import service from "../appwrite/config";
+import { Link } from "react-router-dom";
+
+function PostCard({ $id, title, featuredImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="bg-gray-100 w-full rounded-xl p-4">
+        <div className="w-fyll justify-center mb-4">
+          <img
+            src={service.getFilePreview(featuredImage)}
+            alt={title}
+            srcset=""
+          />
+        </div>
+        <h2 className="text-xl font-bold ">{title}</h2>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -4,5 +4,7 @@ import Header from "./Header/Header";
 import Logo from "./Logo";
 import Input from "./Input";
 import Select from "./Select";
+import PostCard from "./PostCard";
+import Button from "./Button";
 
-export { Header, Footer, Container, Logo, Input, Select };
+export { Header, Footer, Container, Logo, Input, Select, PostCard, Button };
